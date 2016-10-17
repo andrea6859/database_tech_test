c371a1cb333081407db695acf288c827a29d451b
Andrea Wildsmith <awildsmi@hotmail.com>
2016-10-17 17:11:16 +0100

    includes set and get routes

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,26 @@
 require 'sinatra/base'
 
+
 class Database < Sinatra::Base
+  set :port, 4000
+
+database = {}
+
   get '/' do
-    'Hello database_tech_test!'
+    'Hello'
+  end
+
+  get '/set' do
+    database.merge!(params)
+    puts database
+  end
+
+  get '/get' do
+    value = database.values
+    puts value
+
+
+
   end
 
   # start the server if ruby file executed directly
